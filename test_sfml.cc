84e06ee1c9efc856ad7faf1312d660e8b9fb13fe
BastienG27 <bastien.guillemare@gmail.com>
2022-12-14 17:13:51 +0100

    texture cheval

--- test_sfml.cc
+++ test_sfml.cc
@@ -1,9 +1,26 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
+
+#define WIDTH 800
+#define HEIGHT 600
+
+
 int main()
-{
+{   
+
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My window");
+
+    sf::Texture texture;
+    sf::Sprite sprite;
+
+    if (!texture.loadFromFile("logo_cheval.png", sf::IntRect(0, 0, HEIGHT, HEIGHT))){
+        std::cout << "Erreur loading img\n";
+    }
+    texture.setSmooth(true);
+    sprite.setTexture(texture);
+    
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -19,7 +36,7 @@ int main()
 
         // clear the window with black color
         window.clear(sf::Color::Black);
-
+        window.draw(sprite);
         // draw everything here...
         // window.draw(...);
 
