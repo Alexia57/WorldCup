eba24ac53a33f2f171d3013e006eaa9ab9a5af1e
BastienG27 <bastien.guillemare@gmail.com>
2023-01-15 17:00:22 +0100

    merge useful file

--- src/Useful.cpp
+++ src/Useful.cpp
@@ -1,4 +1,4 @@
-#include "usefull.hpp"
+#include "Useful.hpp"
 
 // compare si 2 listes sont identiques et return true si oui
 bool compare_lists(std::list<std::string> lst1, std::list<std::string> lst2){
--- src/Useful.hpp
+++ src/Useful.hpp
@@ -2,8 +2,21 @@
 #define USEFUL_HPP
 
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include <vector>
+#include <list>
+
 #include <cmath> //round
+#include <SFML/Graphics.hpp>
+
+#include "Question.hpp"
+#include "../lib/pugixml/pugixml.hpp"
+
+
+// compare si 2 listes sont identiques et return true si oui
+bool compare_lists(std::list<std::string> lst1, std::list<std::string> lst2);
+
+// fonction pour récupérer les données d'un fichier XML dans des variables de la classe Question
+std::vector<Question> parseXML(const std::string& fileName);
 
 class Useful {
 public:
